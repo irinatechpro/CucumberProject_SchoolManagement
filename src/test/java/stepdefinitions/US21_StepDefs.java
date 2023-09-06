c107677b87a38b4912c276563ed09c9016e109e1
Aynura <aynura.ahmadova@googlemail.com>
2023-09-06 16:26:17 +0100

    updated US21

--- src/test/java/stepdefinitions/US21_StepDefs.java
+++ src/test/java/stepdefinitions/US21_StepDefs.java
@@ -38,21 +38,35 @@ public class US21_StepDefs {
     public void verify_teacher_day_start_time_and_stop_time_are_visible() {
         assertTrue(studentChooseLessonPage.teacherHeader.isDisplayed());
     }
-    @Then("user selects Java and API")
-    public void user_selects_java_and_api() {
-        JSUtils.clickWithTimeoutByJS(studentChooseLessonPage.lessonsListColumnEnglish);
+    @Then("user selects a lesson")
+    public void user_selects_a_lesson() {
+        WaitUtils.waitFor(2);
+        JSUtils.scrollIntoViewJS(studentChooseLessonPage.selectedLessonCheckBox1);
+        JSUtils.clickWithTimeoutByJS(studentChooseLessonPage.selectedLessonCheckBox1);
     }
     @Then("user clicks on submit button")
     public void user_clicks_on_submit_button() {
-        studentChooseLessonPage.submitButton.click();
+        JSUtils.clickWithTimeoutByJS(studentChooseLessonPage.submitButton);
     }
+    //TC01 FIX!!!!!!!!!!!!!!
     @Then("verify lesson added to student message appear")
     public void verify_lesson_added_to_student_message_appear() {
-
+        //WaitUtils.waitForVisibility(studentChooseLessonPage.lessonAddedToStudentAlertMessage,5);
+        assertTrue(studentChooseLessonPage.lessonAddedToStudentAlertMessage.getText().contains("Lesson added to student"));
     }
+    //COMPLETE!!!!!!!!
     @Then("verify selected courses are visible in lesson program")
     public void verify_selected_courses_are_visible_in_lesson_program() {
 
     }
+
+    //TC02
+    @Then("verify course schedule cannot be selected for the same hour and day")
+    public void verify_course_schedule_cannot_be_selected_for_the_same_hour_and_day() {
+        WaitUtils.waitForVisibility(studentChooseLessonPage.collisionErrorAlertMessage,5);
+        assertTrue(studentChooseLessonPage.collisionErrorAlertMessage.getText().contains("Course schedule cannot be selected for the same hour and day"));
+    }
+
+    //TC03
 
 }
--- src/test/resources/features/US21.feature
+++ src/test/resources/features/US21.feature
@@ -2,34 +2,26 @@
 Feature: student choose lesson management panel
   Background: Go_to_Homepage
     Given user navigates to "https://managementonschools.com/"
-  @US21_TC01
-  Scenario Outline: Students choose lesson, grades and announcement successfully
     And user clicks on login link
-    And user enters username "<username>"
-    And user enters password "<password>"
+    And user enters username "robertsmith"
+    And user enters password "Abc12345"
     And user clicks on login button
+  @US21_TC01
+  Scenario : Students choose lesson, grades and announcement successfully
     Then verify Teacher, Day, Start Time and Stop Time are visible
-    And user selects Java and API
+    And user selects a lesson
     And user clicks on submit button
     Then verify lesson added to student message appear
     Then verify selected courses are visible in lesson program
     Then close the application
-    Examples: test_data
-      | username    | password |
-      | robertsmith | Abc12345 |
+
 
   @US21_TC02
-  Scenario Outline: Students choose lesson with same day and time
-  user clicks on login link
-    And user enters name "<username>"
-    And user enters valid email address "<password>"
-    And user clicks on login button
+  Scenario : Students choose lesson with same day and time
     Then verify Teacher, Day, Start Time and Stop Time are visible
-    And user selects English and Java
+    And user selects a lesson
     And user clicks on submit button
     Then verify course schedule cannot be selected for the same hour and day
     Then close the application
-    Examples: test_data
-      | username    | password |
-      | robertsmith | Abc12345 |
+
 
