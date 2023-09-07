ed1f970c954703305491ac22515c8ac9e62d51cc
Aynura <aynura.ahmadova@googlemail.com>
2023-09-07 18:34:47 +0100

    upload US21 final version

--- src/test/java/pages/StudentChooseLessonPage.java
+++ src/test/java/pages/StudentChooseLessonPage.java
@@ -22,19 +22,34 @@ public class StudentChooseLessonPage {
     @FindBy(xpath = "(//th[.='Stop Time'])[1]")
     public WebElement stopTimeHeader;
 
-    @FindBy(xpath = "//input[@value='130']")
+    @FindBy(xpath = "//input[@value='335']")
     public WebElement selectedLessonCheckBox1;
 
     @FindBy(xpath = "//button[.='Submit']")
     public WebElement submitButton;
 
     @FindBy(xpath = "(//tbody)[2]/tr/td[1]")
-    public WebElement lessonsProgramListColumn;
+    public WebElement addedlessonsList;
 
     @FindBy(xpath = "//div[@role='alert']")
     public WebElement lessonAddedToStudentAlertMessage;
 
     @FindBy(xpath = "//div[@role='alert']")
     public WebElement collisionErrorAlertMessage;
+
+    @FindBy(xpath = "(//tbody)[2]/tr/td[1]/*[.='Cypress']")
+    public WebElement addedLesson;
+
+    @FindBy(xpath = "//*[.='Menu']")
+    public WebElement menuButton;
+
+    @FindBy(xpath = "//*[.='Grades and Announcements']")
+    public WebElement gradesAndAnnouncementsLink;
+
+    @FindBy(xpath = "(//tbody[@class='table-group-divider'])[1]")
+    public WebElement studentGradeList;
+
+    @FindBy(xpath = "(//tbody[@class='table-group-divider'])[2]")
+    public WebElement studentMeetingList;
 
 }
