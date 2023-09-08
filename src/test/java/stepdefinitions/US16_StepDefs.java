57e2e7ba689a0bc30e92b510222af2eac8e28d0e
Iryna R <mnops@ukr.net>
2023-09-08 13:42:06 +0100

    US16 complete

--- src/test/java/stepdefinitions/US16_StepDefs.java
+++ src/test/java/stepdefinitions/US16_StepDefs.java
@@ -6,57 +6,69 @@ import io.cucumber.java.en.Then;
 import pages.DeanContactGetAllPage;
 import pages.ViceDean_AdminManagementPage;
 import pages.ViceDean_LessonManagementPage;
-
+import utilities.BrowserUtils;
+import utilities.MediaUtils;
+import java.io.IOException;
 import static utilities.WaitUtils.waitFor;
 
 public class US16_StepDefs {
     ViceDean_AdminManagementPage viceDeanAdminManagementPage= new ViceDean_AdminManagementPage();
-   ViceDean_LessonManagementPage viceDean_lessonManagementPage = new ViceDean_LessonManagementPage();
     DeanContactGetAllPage deanContactGetAllPage = new DeanContactGetAllPage();
+
    @And("click Menu button")
    public void clickMenuButton() {
       waitFor(2);
       viceDeanAdminManagementPage.navMenuButton.click();
-      waitFor(2);
+      waitFor(1);
   }
-    @And("click Contact Get All button")
-    public void click_contact_get_all_button() {
+    @And("click Get All button")
+    public void contact_get_all_button() {
         waitFor(2);
-        de
+        deanContactGetAllPage.contactGetAllButton.click();
         waitFor(2);
-
    }
+   // US16_TC01
     @Then("Verify Delete button is visible")
     public void verify_delete_button_is_visible() {
-
+        try {
+            MediaUtils.takeScreenshotOfTheEntirePage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-    @Then("verify message is seen")
+   // US16_TC02
+    @Then("verify message is visible")
     public void verify_message_is_seen() {
-
+        waitFor(3);
+        BrowserUtils.verifyElementDisplayed(deanContactGetAllPage.verifyMessageIsSeen);
     }
-    @Then("verify Name is seen")
-    public void verify_name_is_seen() {
-
+    @Then("verify Name is visible")
+    public void verify_name_is_visible() {
+       BrowserUtils.verifyElementDisplayed(deanContactGetAllPage.verifyNameIsSeen);
+        waitFor(3);
     }
-    @Then("verify Email is seen")
-    public void verify_email_is_seen() {
-
+    @Then("verify Email is visible")
+    public void verify_email_is_visible() {
+        BrowserUtils.verifyElementDisplayed(deanContactGetAllPage.verifyEmailIsSeen);
+        waitFor(3);
     }
-    @Then("verify Date is seen")
-    public void verify_date_is_seen() {
-
+    @Then("verify Date is visible")
+    public void verify_date_is_visible() {
+        waitFor(3);
+        BrowserUtils.verifyElementDisplayed(deanContactGetAllPage.verifyDateIsSeen);
     }
-    @Then("verify Subject is seen")
-    public void verify_subject_is_seen() {
-
+    @Then("verify Subject is visible")
+    public void verify_subject_is_visible() {
+        waitFor(3);
+        BrowserUtils.verifyElementDisplayed(deanContactGetAllPage.verifySubjectIsSeen);
     }
-
-    @Then("verify message is deleted")
+   //US16_TC03
+    @Then("verify ability to delete messages")
     public void verify_message_is_deleted() {
-
+        try {
+            MediaUtils.takeScreenshotOfTheEntirePage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-
-
 }
--- src/test/resources/features/US16.feature
+++ src/test/resources/features/US16.feature
@@ -4,27 +4,20 @@ Feature:The Vice Dean should be able to view the messages sent by users.
   Background: Login As Vice Dean
     Given login as vice dean "Agent07" "11111111eW"
     And click Menu button
-    And click Contact Get All button
-
+    And click Get All button
 
   @US16_TC01
   Scenario: The Vice Dean should be able to see the delete button.
-#    And click Menu button
-#    And click Contact Get All button
     Then Verify Delete button is visible
 
   @US16_TC02
   Scenario:The Vice Dean should be able to view the messages, their authors, emails, sending date and subject information
-    And click Menu button
-    And click Contact Get All button
-    Then verify message is seen
-    Then verify Name is seen
-    Then verify Email is seen
-    Then verify Date is seen
-    Then verify Subject is seen
+    Then verify message is visible
+    Then verify Name is visible
+    Then verify Email is visible
+    Then verify Date is visible
+    Then verify Subject is visible
 
   @US16_TC03
   Scenario: The Vice Dean should be able to delete messages.
-    And click Menu button
-    And click Contact Get All button
-    Then verify message is deleted
+    Then verify ability to delete messages
