de1ca3b9314ac694eb4cd42ce8f6d7c219d5e2aa
Gaffari Oguz <gaffarieduit@gmail.com>
2023-09-10 22:43:01 +0200

    US20 complete version including TC02 and TC03

--- src/test/java/pages/TeacherMeetManagementPage.java
+++ src/test/java/pages/TeacherMeetManagementPage.java
@@ -32,4 +32,35 @@ public class TeacherMeetManagementPage {
     @FindBy(xpath = "//form")
     public WebElement verifyElementsAtAddMeetAreEdited;
 
+    @FindBy(xpath = "//table//tr[1]/td[2]")
+    public WebElement verifyMeetDateIsVisible;
+    @FindBy(xpath = "//table//tr[1]/td[3]")
+    public WebElement verifyStartTimeIsVisible;
+    @FindBy(xpath = "//table//tr[1]/td[4]")
+    public WebElement verifyStopTimeIsVisible;
+    @FindBy(xpath = "//table//tr[1]/td[5]")
+    public WebElement verifyDescriptionIsVisible;
+    @FindBy(xpath = "//table//tr[1]/td[6]")
+    public WebElement editButton;
+    @FindBy(id = "description")
+    public WebElement descriptionUpdateField;
+//"(//input[@class='form-control'])[4]"
+    @FindBy(xpath = "(//button[@class='fw-semibold btn btn-primary btn-lg'])[2]")
+    public WebElement updateSubmitButton;
+
+    @FindBy(xpath = "/html/body/div[4]/div/div/div[2]/div[1]/div/div/form/div/div[1]/div/div/div[1]/div[2]")
+    public WebElement updateStudentField;
+
+    @FindBy(xpath = "//div[contains(text(), 'Meet Updated Successfully')]")
+    public WebElement verifyMeetUpdatedMessage;
+
+    @FindBy(xpath = "//table//tr[1]/td[7]")
+    public WebElement deleteMeetButton;
+
+    @FindBy(xpath = "//div[contains(text(), 'Meet deleted successfully')]")
+    public WebElement verifyMeetDeletedMessage;
+
+
+
+
 }
