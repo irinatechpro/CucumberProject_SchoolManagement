eab9bbd7529192a5b4ca66d1231e80e3d54c2b79
Nalan Orhan <karadumannalan@gmail.com>
2023-09-12 14:24:12 +0200

    update framework2

--- src/test/java/utilities/Authentication.java
+++ src/test/java/utilities/Authentication.java
@@ -10,15 +10,61 @@ import static io.restassured.RestAssured.given;
 
 public class Authentication {
 
-    public static String generateToken(String username, String password) {
-        String url= ConfigReader.getProperty("base_url") + "/auth/login";
-        Map<String, Object>token=new HashMap<>();
-        token.put( "password", password);
-        token.put("username", username);
-        Response response=given().contentType(ContentType.JSON).body(token).post(url);
-        response.prettyPrint();
-        return response.jsonPath().getString("token");
-
+    static   String url = ConfigReader.getProperty("authenticationURL");
+    //admin
+    public static String generateTokenAdmin(){
+        Map<String, Object> body = new HashMap<>();
+        body.put("username",ConfigReader.getProperty("adminUserName"));
+        body.put("password",ConfigReader.getProperty("adminPassword"));
+        body.put("rememberMe",true);
+        Response response = given().contentType(ContentType.JSON).body(body).post(url);
+        return  response.jsonPath().getString("token");
     }
+    //dean
+    public static String generateTokenDean(){
+        Map<String, Object> body = new HashMap<>();
+        body.put("username",ConfigReader.getProperty("deanUserName"));
+        body.put("password",ConfigReader.getProperty("deanPassword"));
+        body.put("rememberMe",true);
+        Response response = given().contentType(ContentType.JSON).body(body).post(url);
+        return  response.jsonPath().getString("token");
+    }
+    //vice dean
+    public static String generateTokenViceDean() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("username", ConfigReader.getProperty("viceDeanUserName"));
+        body.put("password", ConfigReader.getProperty("viceDeanPassword"));
+        body.put("rememberMe",true);
+        Response response = given().contentType(ContentType.JSON).body(body).post(url);
+        return response.jsonPath().getString("token");
+    }
+    //teacher
+    public static String generateTokenTeacher() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("username", ConfigReader.getProperty("teacherUserName"));
+        body.put("password", ConfigReader.getProperty("teacherPassword"));
+        body.put("rememberMe",true);
+        Response response = given().contentType(ContentType.JSON).body(body).post(url);
+        return response.jsonPath().getString("token");
+    }
+    //student
+    public static String generateTokenStudent(){
+        Map<String, Object> body = new HashMap<>();
+        body.put("username",ConfigReader.getProperty("studentUserName"));
+        body.put("password",ConfigReader.getProperty("studentPassword"));
+        body.put("rememberMe",true);
+        Response response = given().contentType(ContentType.JSON).body(body).post(url);
+        return  response.jsonPath().getString("token");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("generateTokenAdmin() = " + generateTokenAdmin());
+        System.out.println("generateTokenDean() = " + generateTokenDean());
+        System.out.println("generateTokenViceDean() = " + generateTokenViceDean());
+        System.out.println("generateTokenTeacher() = " + generateTokenTeacher());
+        System.out.println("generateTokenStudent() = " + generateTokenStudent());
+    }
+
 
 }
---
+++ src/test/resources/features/e2eFeatures/e2e_US01.feature
@@ -0,0 +1,27 @@
+@E2E @US01
+Feature:Prospective students should be able to register in the system
+
+  @UI
+  Scenario Outline: user register successfully
+    Given user navigates to "https://managementonschools.com/"
+    And user clicks on register link
+    And user enters name "Anna"
+    And user enters surname "<surname>"
+    And user enters birthplace "San Antonio"
+    And enter valid phone number
+    And user clicks on radio button
+    And user enter date of birth "01/01/2000"
+    And enter valid SSN
+    And user enters user name "<username>"
+    And user enters password "Anna1234&"
+    And user clicks on register button
+    Then verify Guest User registered alert is seen
+
+    Examples: test_data
+      | name | surname    | birth_place      | phone_number  | date_of_birth | ssn_number  | username | password  |
+      | Anna | Silver     | San Antonio      | 123-321-1243  | 01/01/2000    | 187-89-9765 | Anna     | Anna2323! |
+  @API
+  Scenario:
+
+  @DB
+  Scenario:
