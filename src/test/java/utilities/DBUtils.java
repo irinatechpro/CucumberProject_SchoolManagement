024984191f94c82667278a037bf50d2210adf531
Nalan Orhan <karadumannalan@gmail.com>
2023-09-18 23:00:40 +0200

    DButils class update

--- src/test/java/utilities/DBUtils.java
+++ src/test/java/utilities/DBUtils.java
@@ -1,22 +1,12 @@
 package utilities;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 public class DBUtils {
-    private static Connection connection;
-    private static Statement statement;
-    private static ResultSet resultSet;
-    /**
-     * DBUtils.createConnection(); -> to connect to teh database
-     */
+
+
     //This method establishes a connection with the ManagementOnSchool database and returns Connection data
     public static Connection connectToDatabase() {
         Connection connection;
@@ -27,179 +17,69 @@ public class DBUtils {
         }
         return connection;
     }
-    /**
-     * DBUtils.executeQuery(String query); -> Execute the query and store is the result set object
-     * STATEMENT : is used to write query
-     */
-    public static Statement getStatement() {
+
+
+    //This method creates and returns a statement object by calling the connectToDatabase() method
+    public static Statement createStatement() {
+        Statement statement;
         try {
-            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            statement = connectToDatabase().createStatement();
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return statement;
     }
-    public static ResultSet getResultset() {
-        try {
-            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return resultSet;
-    }
-    public static ResultSet executeQuery(String query) {
-        try {
-            return getStatement().executeQuery(query);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+
+
     //This method runs a SQL query and returns true if data is returned, false otherwise
     public static boolean execute(String sql) {
+
         try {
-            return getStatement().execute(sql);
+            return createStatement().execute(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
     }
-    // This method returns the number fo row in a table in the database
-    public static int getRowCount() throws Exception {
-        resultSet.last();
-        int rowCount = resultSet.getRow();
-        return rowCount;
-    }
-    /**
-     * @return returns a single cell value. If the results in multiple rows and/or
-     * columns of data, only first column of the first row will be returned.
-     * The rest of the data will be ignored
-     */
-    public static Object getCellValue(String query) {
-        return getQueryResultList(query).get(0).get(0);
-    }
-    /**
-     * @return returns a list of Strings which represent a row of data. If the query
-     * results in multiple rows and/or columns of data, only first row will
-     * be returned. The rest of the data will be ignored
-     */
-    public static List<Object> getRowList(String query) {
-        return getQueryResultList(query).get(0);
-    }
-    /**
-     * @return returns a map which represent a row of data where key is the column
-     * name. If the query results in multiple rows and/or columns of data,
-     * only first row will be returned. The rest of the data will be ignored
-     */
-    public static Map<String, Object> getRowMap(String query) {
-        return getQueryResultMap(query).get(0);
-    }
-    /**
-     * @return returns query result in a list of lists where outer list represents
-     * collection of rows and inner lists represent a single row
-     */
-    public static List<List<Object>> getQueryResultList(String query) {
-        executeQuery(query);
-        List<List<Object>> rowList = new ArrayList<>();
-        ResultSetMetaData rsmd;
+
+
+    //This method runs a SQL query and returns the result as ResultSet
+    public static ResultSet executeQuery(String sql) {
+
         try {
-            rsmd = resultSet.getMetaData();
-            while (resultSet.next()) {
-                List<Object> row = new ArrayList<>();
-                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                    row.add(resultSet.getObject(i));
-                }
-                rowList.add(row);
-            }
+            return createStatement().executeQuery(sql);
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return rowList;
+
     }
-    /**
-     * @return list of values of a single column from the result set
-     */
-    public static List<Object> getColumnData(String query, String column) {
-        executeQuery(query);
-        List<Object> rowList = new ArrayList<>();
-        ResultSetMetaData rsmd;
-        try {
-            rsmd = resultSet.getMetaData();
-            while (resultSet.next()) {
-                rowList.add(resultSet.getObject(column));
-            }
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return rowList;
-    }
-    /**
-     * @return returns query result in a list of maps where the list represents
-     * collection of rows and a map represents a single row with
-     * key being the column name
-     */
-    public static List<Map<String, Object>> getQueryResultMap(String query) {
-        executeQuery(query);
-        List<Map<String, Object>> rowList = new ArrayList<>();
-        ResultSetMetaData rsmd;
-        try {
-            rsmd = resultSet.getMetaData();
-            while (resultSet.next()) {
-                Map<String, Object> colNameValueMap = new HashMap<>();
-                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                    colNameValueMap.put(rsmd.getColumnName(i), resultSet.getObject(i));
-                }
-                rowList.add(colNameValueMap);
-            }
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return rowList;
-    }
-    /*
-     * @return List of columns returned in result set
-     */
-    public static List<String> getColumnNames(String query) {
-        executeQuery(query);
-        List<String> columns = new ArrayList<>();
-        ResultSetMetaData rsmd;
-        try {
-            rsmd = resultSet.getMetaData();
-            int columnCount = rsmd.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                columns.add(rsmd.getColumnName(i));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return columns;
-    }
+
     //This method returns a column of any table we want as a list
     public static List<Object> getColumnList(String tableName, String columnName) throws SQLException {
+
         List<Object> list = new ArrayList<>();
-        resultSet = executeQuery("select " + columnName + " from " + tableName);
+
+        ResultSet resultSet = executeQuery("select " + columnName + " from " + tableName);
+
         while (resultSet.next()) {
             list.add(resultSet.getObject(columnName));
         }
+
         return list;
+
     }
-    //    used to close the connectivity
+
+    //This method closes the connection
     public static void closeConnection() {
+
         try {
-            if (resultSet != null) {
-                resultSet.close();
-            }
-            if (statement != null) {
-                statement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
+            connectToDatabase().close();
+            createStatement().close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+
     }
+
+
 }
