d07cfb7092f46c07f7314bdaf4fbad25cb17fa34
Eylem Sahinturk Pinkney <eylempinkney@gmail.com>
2023-10-03 11:04:15 +0100

    Update 2rd Oct

--- src/test/java/stepdefinitions/uiStepDefinitions/US25_StepDefs.java
+++ src/test/java/stepdefinitions/uiStepDefinitions/US25_StepDefs.java
@@ -3,6 +3,7 @@ package stepdefinitions.uiStepDefinitions;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import pages.AdminManagementPage;
+import utilities.DBUtils;
 
 public class US25_StepDefs {
 
@@ -12,6 +13,12 @@ public class US25_StepDefs {
     public void user_sees_title(String string) {
         Assert.assertTrue(adminManagementPage.adminManagementTitle.getText().contains("Admin Management"));
     }
+
+    @Then("close the connection")
+    public void close_the_connection() {
+        DBUtils.closeConnection();
+    }
+
 
 
 }
